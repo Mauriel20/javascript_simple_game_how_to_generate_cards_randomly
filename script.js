c4d802a45e772aa869d52546068818949f78d4bb
Mauriel <josueazofeifajosue@gmail.com>
2021-01-28 23:41:59 +0000

    My-work

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let cards = ['♥', '♣', '♦', '♠'];
-let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']; 
+let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 
 function displayCard() {
-  let singleCard = cards[Math.floor(Math.random() * cards.length)];
-  let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
-  let showCard = `${singleNumber}   ${singleCard}`;
-  document.getElementById("showing").style.background = '#0accff';
-  document.getElementById("showing").innerHTML = showCard;
+    
+    let numerosize = document.querySelector('#icono-1');
+    let numerosize2 = document.querySelector('#icono-2');
+    let singleCard = cards[Math.floor(Math.random() * cards.length)];
+    let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+    let showCard = `${singleNumber}`;
+    let icon1 = `${singleCard}`;
+    
+    numerosize.style.fontSize = "90px";
+    numerosize2.style.fontSize = "90px";
+    document.getElementById("numero").innerHTML = showCard;
+    document.getElementById("icono-1").innerHTML = icon1;
+    document.getElementById("icono-2").innerHTML = icon1;
 
 }
 
