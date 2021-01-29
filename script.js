6ef55b401faafc1cd9e4dbdbc785f875de695501
Mauriel <josueazofeifajosue@gmail.com>
2021-01-29 04:53:08 +0000

    ready2

--- script.js
+++ script.js
@@ -8,14 +8,34 @@ function displayCard() {
     let numerosize2 = document.querySelector('#icono-2');
     let singleCard = cards[Math.floor(Math.random() * cards.length)];
     let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
-    let showCard = `${singleNumber}`;
-    let icon1 = `${singleCard}`;
+   
     
     numerosize.style.fontSize = "90px";
     numerosize2.style.fontSize = "90px";
-    document.getElementById("numero").innerHTML = showCard;
-    document.getElementById("icono-1").innerHTML = icon1;
-    document.getElementById("icono-2").innerHTML = icon1;
+
+      document.getElementById("numero").innerHTML = singleNumber;
+    document.getElementById("icono-1").innerHTML = singleCard;
+    document.getElementById("icono-2").innerHTML = singleCard;
+    
+    if( singleCard == '♥'){
+        document.querySelector("#icono-1").style.color = "red";
+        document.querySelector("#icono-2").style.color = "red";
+    }
+    if( singleCard == '♦'){
+        document.querySelector("#icono-1").style.color = "red";
+        document.querySelector("#icono-2").style.color = "red";
+    }
+    if( singleCard == '♣' ){
+        document.querySelector("#icono-1").style.color = "black";
+        document.querySelector("#icono-2").style.color = "black";
+    }
+    if( singleCard == '♠' ){
+        document.querySelector("#icono-1").style.color = "black";
+        document.querySelector("#icono-2").style.color = "black";
+    }
+    
+  
+     
 
 }
 
